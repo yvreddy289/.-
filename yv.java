69c3626c1a925929a06b3df5534cc0645ce2da02
yvreddy289 <yvreddy2k14@gmail.com>
2017-03-07 14:56:53 -0500

    my code

---
+++ yv.java
@@ -0,0 +1,13 @@
+package org.yv.mentis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
